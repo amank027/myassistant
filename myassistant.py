0ce5a42666967e002c28c33acbd7009587da2c35
khandelwal027 <56469110+khandelwal027@users.noreply.github.com>
2020-09-30 22:53:07 +0530

    Update myassistant.py

--- myassistant.py
+++ myassistant.py
@@ -9,43 +9,43 @@ while True:
     r = input()
     
     if (('run' in r) or ('start' in r) or  ('open' in r) or ('execute' in r)) and (('chrome' in r) or ('browser' in r)):
-        pyttsx3.speak("opening google chrome")
+        pyttsx3.speak("opening google chrome for you")
         os.system("start chrome")
                                                                                     
     elif (('run' in r) or ('start' in r) or ('open' in r) or ('execute' in r)) and (('media' in r) or ('player' in r) or ('windows media player' in r)):
-        pyttsx3.speak('opening windows media player')
+        pyttsx3.speak('opening windows media player for you')
         os.system('start wmplayer')
         
     elif (('run' in r) or ('start' in r)  or ('open' in r) or ('execute' in r)) and (('notepad' in r) or ('notebook' in r) or ('text editor' in r)):
-        pyttsx3.speak("opening notepad")
+        pyttsx3.speak("opening notepad for you")
         os.system('start notepad')
         
     elif (('run' in r) or ('start' in r) or ('open' in r) or ('execute' in r)) and ('paint' in r):
-        pyttsx3.speak('opening paint')
+        pyttsx3.speak('opening paintfor you')
         os.system('start mspaint')
         
     elif (('run' in r) or ('start' in r) or ('open' in r) or ('execute' in r)) and (('cmd' in r) or ('command prompt' in r) or ('command' in r)):
-        pyttsx3.speak('opening command prompt')
+        pyttsx3.speak('opening command prompt for you')
         os.system('start cmd.exe')
 
     elif (('run' in r) or ('start' in r)  or ('open' in r) or ('execute' in r)) and (('panel' in r) or ('control' in r) or ('control panel' in r)):
-        pyttsx3.speak('opening control panel')
+        pyttsx3.speak('opening control panel for you')
         os.system('start control panel')      
                 
     elif (('run' in r) or ('start' in r)  or ('open' in r) or ('execute' in r)) and (('calculator' in r) or ('calc' in r)):
-        pyttsx3.speak('opening calculator')
+        pyttsx3.speak('opening calculator for you')
         os.system('start calc')
 
     elif (('run' in r) or ('start' in r) or ('open' in r) or ('execute' in r)) and (("excel" in r) or ('microsoft excel') in r ):
-        pyttsx3.speak('opening microsoft excel')
+        pyttsx3.speak('opening microsoft excel for you')
         os.system('start excel.exe')
 
     elif (('run'in r) or ('start' in r) or ('open' in r) or ('execute' in r)) and (('power point' in r) or ('microsoft power point' in r)):
-        pyttsx3.speak('opening microsoft powerpoint')
+        pyttsx3.speak('opening microsoft powerpoint for you')
         os.system('start powerpnt')
 
     elif (('run'in r) or ('start' in r) or ('open' in r) or ('execute' in r)) and (('explorer' in r) or ('internet explorer' in r)):
-        pyttsx3.speak('opening internet explorer')
+        pyttsx3.speak('opening internet explorer for you')
         os.system('start iexplore')
 
     elif (('stop' in r) or ('exit' in r) or ('quit' in r) or ('close' in r)):
